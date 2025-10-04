04cbea49b672366efbce4a7d87f0cf96cd40538d
javihen <62959381+javihen@users.noreply.github.com>
2025-10-03 20:52:31 -0400

    feat formulario de login

--- electroReg/Form1.cs
+++ electroReg/Form1.cs
@@ -16,5 +16,15 @@ namespace electroReg
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
